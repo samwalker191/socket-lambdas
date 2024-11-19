676626d0230c264242a1856c1dfa8b672f6b0041
Sam Walker <sewalker191@gmail.com>
2024-11-18 18:12:12 -0800

    remove connected bool, add BsonIgnoreIfDefault due to upsert defaulting object id to all 0s

--- src/Domain/Client.cs
+++ src/Domain/Client.cs
@@ -1,14 +1,14 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Domain;
 
-public class Client(string connectionId, bool connected)
+public class Client(string connectionId)
 {
+    [BsonIgnoreIfDefault]
     public ObjectId Id { get; set; }
     
     public string ConnectionId { get; set; } = connectionId;
-
-    public bool Connected { get; set; } = connected;
     
     public List<string> Subscriptions { get; set; } = new();
 }
