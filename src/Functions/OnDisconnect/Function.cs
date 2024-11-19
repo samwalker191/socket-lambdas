e3ffdb3f6e57b32221de50f12a025fa8b5b32f27
Sam Walker <sewalker191@gmail.com>
2024-11-19 14:07:54 -0800

    update OnDisconnect function to use ClientConnectionService

--- src/Functions/OnDisconnect/Function.cs
+++ src/Functions/OnDisconnect/Function.cs
@@ -2,6 +2,8 @@
 using Amazon.Lambda.Core;
 using Domain;
 using MongoDB.Driver;
+using Services;
+using Services.Interfaces;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -10,41 +12,28 @@ namespace OnDisconnect
 {
     public class Function
     {
-        private static MongoClient? _mongoClient;
-
-        private static MongoClient CreateMongoClient()
+        private static IClientConnectionService _clientConnectionService = null!;
+        
+        // Lambda invokes a parameterless constructor function when warming up
+        // Structuring it this way allows for mocks to be used for testing more easily
+        public Function(): this(null)
         {
-            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-            if (string.IsNullOrEmpty(connectionString)) throw new ApplicationException("DB_CONNECTION_STRING environment variable is not defined");
-            return new MongoClient(connectionString);
         }
 
-        static Function()
+        public Function(IClientConnectionService? clientConnectionService)
         {
-            _mongoClient = CreateMongoClient();
+            _clientConnectionService = clientConnectionService ?? new ClientConnectionService();
         }
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            if (_mongoClient == null)
-            {
-                context.Logger.LogError("Database client is not initialized");
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 400,
-                    Body = "Failed to disconnect"
-                };
-            }
-            
-            var database = _mongoClient.GetDatabase("rebuild-test");
-            var clientCollection = database.GetCollection<Client>("clients");
             try
             {
                 var connectionId = request.RequestContext.ConnectionId;
                 context.Logger.LogLine($"ConnectionId: {connectionId}");
                 
-                var result = await clientCollection.DeleteOneAsync(x => x.ConnectionId == connectionId);
+                var result = await _clientConnectionService.DeleteClientConnectionByConnectionId(connectionId);
                 
-                if (!result.IsAcknowledged) return new APIGatewayProxyResponse
+                if (!result) return new APIGatewayProxyResponse
                     {
                         StatusCode = 400,
                         Body = "Unable to disconnect"
