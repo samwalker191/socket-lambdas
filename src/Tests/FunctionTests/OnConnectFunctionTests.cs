1dbfe43018a36cb32a39144c2d17d8c4fb5610d9
Sam Walker <sewalker191@gmail.com>
2024-11-19 13:55:23 -0800

    add initial tests for client connection service and OnConnect function

---
+++ src/Tests/FunctionTests/OnConnectFunctionTests.cs
@@ -0,0 +1,96 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.TestUtilities;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json;
+using OnConnect;
+using Services.Interfaces;
+using Xunit;
+
+namespace Tests.FunctionTests;
+
+public class OnConnectFunctionTests
+{
+    public OnConnectFunctionTests()
+    {
+        Environment.SetEnvironmentVariable("DB_CONNECTION_STRING", "mongodb://localhost:27017");
+        Environment.SetEnvironmentVariable("API_KEY", "apikey");
+    }
+
+    [Fact]
+    public async Task ShouldReturnUnauthorizedIfInvalidApiKey()
+    {
+        // Arrange
+        var mockClientConnectionService = new Mock<IClientConnectionService>();
+        var functionToTest = new Function(mockClientConnectionService.Object);
+        
+        var apiGatewayProxyRequestWithBadApiKey = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestWithBadApiKey);
+        var apiGatewayProxyRequestWithoutApiKey = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestWithoutApiKey);
+        var context = new TestLambdaContext();
+        
+        // Act
+        var badKeyResult = await functionToTest.FunctionHandler(apiGatewayProxyRequestWithBadApiKey!, context);
+        var noKeyResult = await functionToTest.FunctionHandler(apiGatewayProxyRequestWithoutApiKey!, context);
+        // Assert
+        badKeyResult.StatusCode.Should().Be(401);
+        badKeyResult.Body.Should().Be("Unauthorized");
+        
+        noKeyResult.StatusCode.Should().Be(401);
+        noKeyResult.Body.Should().Be("Unauthorized");
+    }
+
+    [Fact]
+    public async Task ShouldReturnBadRequestIfFailedToCreateClientConnection()
+    {
+        // Arrange
+        var mockClientConnectionService = new Mock<IClientConnectionService>();
+        mockClientConnectionService.Setup(x => x.CreateClientConnection("banana")).ReturnsAsync(false);
+        var functionToTest = new Function(mockClientConnectionService.Object);
+        
+        var apiGatewayProxyRequest = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestWithApiKey);
+        var context = new TestLambdaContext();
+        
+        // Act
+        var failedResult = await functionToTest.FunctionHandler(apiGatewayProxyRequest!, context);
+        // Assert
+        failedResult.StatusCode.Should().Be(400);
+        failedResult.Body.Should().Be("Failed to connect");
+    }
+    
+    [Fact]
+    public async Task ShouldCatchErrorsOnAttemptToCreateClientConnectionAndReturnInternalServerErrorResponse()
+    {
+        // Arrange
+        var mockClientConnectionService = new Mock<IClientConnectionService>();
+        mockClientConnectionService.Setup(x => x.CreateClientConnection("banana")).Throws(new Exception());
+        var functionToTest = new Function(mockClientConnectionService.Object);
+        
+        var apiGatewayProxyRequest = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestWithApiKey);
+        var context = new TestLambdaContext();
+        
+        // Act
+        var failedResult = await functionToTest.FunctionHandler(apiGatewayProxyRequest!, context);
+        // Assert
+        failedResult.StatusCode.Should().Be(500);
+        failedResult.Body.Should().Contain("Failed to connect");
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessResponseOnSuccessfulClientConnectionCreation()
+    {
+        // Arrange
+        var mockClientConnectionService = new Mock<IClientConnectionService>();
+        mockClientConnectionService.Setup(x => x.CreateClientConnection("banana")).ReturnsAsync(true);
+        var functionToTest = new Function(mockClientConnectionService.Object);
+        
+        var apiGatewayProxyRequest = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestWithApiKey);
+        var context = new TestLambdaContext();
+        
+        // Act
+        var result = await functionToTest.FunctionHandler(apiGatewayProxyRequest!, context);
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Body.Should().Contain("Connected");
+    }
+}
---
+++ src/Tests/ServiceTests/ClientConnectionServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Domain;
+using FluentAssertions;
+using MongoDB.Driver;
+using Services;
+using Services.Interfaces;
+using Xunit;
+
+namespace Tests.ServiceTests;
+
+public class ClientConnectionServiceTests
+{
+    private readonly IClientConnectionService _clientConnectionService;
+    private readonly IMongoCollection<Client> _clientCollection;
+
+    public ClientConnectionServiceTests()
+    {
+        // set db string to be local test db
+        Environment.SetEnvironmentVariable("DB_CONNECTION_STRING", "mongodb://localhost:27017");
+        _clientConnectionService = new ClientConnectionService();
+        
+        // for assertions 
+        var mongoClient = new MongoClient("mongodb://localhost:27017");
+        _clientCollection = mongoClient.GetDatabase("rebuild-test").GetCollection<Client>("clients");
+    }
+
+    [Fact]
+    public async Task ShouldBeAbleToCreateAClientConnectionAndReturnTrue()
+    {
+        // Arrange
+        var connectionId = "test-connection-id";
+        
+        // Act
+        var result = await _clientConnectionService.CreateClientConnection(connectionId);
+        var clientConnection = _clientCollection.Find(x => x.ConnectionId == connectionId).FirstOrDefault();
+        
+        // Assert
+        clientConnection.ConnectionId.Should().Be(connectionId);
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task ShouldNotCreateAClientConnectionWithEmptyInput()
+    {
+        // Arrange
+        var badConnectionId1 = "";
+        var badConnectionId2 = "      ";
+        
+        
+        // Act
+        var badResult1 = await _clientConnectionService.CreateClientConnection(badConnectionId1);
+        var badResult2 = await _clientConnectionService.CreateClientConnection(badConnectionId1);
+        var clientConnection1 = _clientCollection.Find(x => x.ConnectionId == badConnectionId1).FirstOrDefault();
+        var clientConnection2 = _clientCollection.Find(x => x.ConnectionId == badConnectionId2).FirstOrDefault();
+        
+        // Assert
+        clientConnection1.Should().BeNull();
+        clientConnection2.Should().BeNull();
+        badResult1.Should().BeFalse();
+        badResult2.Should().BeFalse();
+    }
+}
