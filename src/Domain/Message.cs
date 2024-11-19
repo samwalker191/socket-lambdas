701e090a7e5c4d96386382a3f07af784e2f55e7e
Sam Walker <sewalker191@gmail.com>
2024-11-19 00:37:46 -0800

    add PublishMessage class for Publish Function

--- src/Domain/Message.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Domain;
-
-public class SubscribeMessage
-{
-    public string? Topic { get; set; }
-}
---
+++ src/Domain/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain;
+
+public class SubscribeMessage
+{
+    public string? Topic { get; set; }
+}
+
+public class PublishMessage
+{
+    public string? Topic { get; set; }
+    
+    public string? Message { get; set; }
+}
