1b92bb0f45bba37e2a757f7b537ec805352024b4
Sam Walker <sewalker191@gmail.com>
2024-11-19 16:22:51 -0800

    add DefaultFunctionTests

---
+++ src/Tests/FunctionTests/DefaultFunctionTests.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.TestUtilities;
+using Default;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Tests.FunctionTests;
+
+public class DefaultFunctionTests
+{
+    [Fact]
+    public async Task ShouldReturnNotFoundResponse()
+    {
+        // Arrange
+        var functionToTest = new Function();
+        
+        var apiGatewayProxyRequest = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(EventHelper.ApiGatewayProxyRequestDefaultBody);
+        var context = new TestLambdaContext();
+        
+        // Act
+        var result = await functionToTest.FunctionHandler(apiGatewayProxyRequest!, context);
+        
+        // Assert
+        result.StatusCode.Should().Be(404);
+        result.Body.Should().Be("No route found");
+    }
+}
