01800f936ac5e2fc28c7666cc8e8343a85736912
Sam Walker <sewalker191@gmail.com>
2024-11-19 16:18:15 -0800

    Update Publish Function to use ClientConnectionsService

--- src/Functions/Publish/Function.cs
+++ src/Functions/Publish/Function.cs
@@ -6,8 +6,9 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Runtime;
 using Domain;
-using MongoDB.Driver;
 using Newtonsoft.Json;
+using Services;
+using Services.Interfaces;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -16,34 +17,20 @@ namespace Publish
 {
     public class Function
     {
-        private static readonly MongoClient? MongoClient;
-
-        private static MongoClient CreateMongoClient()
+        private static IClientConnectionService _clientConnectionService = null!;
+        
+        // Lambda invokes a parameterless constructor function when warming up
+        // Structuring it this way allows for mocks to be used for testing more easily
+        public Function(): this(null)
         {
-            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-            if (string.IsNullOrEmpty(connectionString)) throw new ApplicationException("DB_CONNECTION_STRING environment variable is not defined");
-            return new MongoClient(connectionString); 
         }
 
-        static Function()
+        public Function(IClientConnectionService? clientConnectionService)
         {
-            MongoClient = CreateMongoClient();
+            _clientConnectionService = clientConnectionService ?? new ClientConnectionService();
         }
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            if (MongoClient == null)
-            {
-                context.Logger.LogError("Database client is not initialized");
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 400,
-                    Body = "Failed to connect"
-                };
-            }
-            
-            var database = MongoClient.GetDatabase("rebuild-test");
-            var clientCollection = database.GetCollection<Client>("clients");
-            
             try
             {
                 var domainName = request.RequestContext.DomainName;
@@ -51,15 +38,15 @@ namespace Publish
                 var endpoint = $"https://{domainName}/{stage}";
                 context.Logger.LogLine($"API Gateway management endpoint: {endpoint}");
                 context.Logger.LogLine($"Body: {request.Body}");
+                
                 var message = JsonConvert.DeserializeObject<PublishMessage>(request.Body);
                 if (message?.Topic == null || message.Message == null) return new APIGatewayProxyResponse
                     {
                         StatusCode = 400,
                         Body = "Unable to process message"
                     };
-
-                var filter = Builders<Client>.Filter.AnyEq(x => x.Subscriptions, message.Topic.ToLower());
-                var clients = await clientCollection.Find(filter).ToListAsync() ?? new List<Client>();
+                
+                var clients = await _clientConnectionService.GetClientsByTopic(message.Topic.ToLower());
                 context.Logger.LogLine($"Found {clients.Count} clients");
                 
                 var stream = new MemoryStream(Encoding.UTF8.GetBytes(message.Message));
@@ -70,31 +57,7 @@ namespace Publish
 
                 foreach (var client in clients)
                 {
-                    var postConnectionRequest = new PostToConnectionRequest
-                    {
-                        ConnectionId = client.ConnectionId,
-                        Data = stream
-                    };
-
-                    try
-                    {
-                        context.Logger.LogLine($"Post to connection: {client.ConnectionId}");
-                        stream.Position = 0;
-                        await apiClient.PostToConnectionAsync(postConnectionRequest);
-                    }
-                    catch (AmazonServiceException e)
-                    {
-                        if (e.StatusCode == HttpStatusCode.Gone)
-                        {
-                            context.Logger.LogLine($"Deleting gone connection: {client.ConnectionId}");
-                            await clientCollection.FindOneAndDeleteAsync(x => x.ConnectionId == client.ConnectionId);
-                        }
-                        else
-                        {
-                            context.Logger.LogLine($"Error posting message to {client.ConnectionId}: {e.Message}");
-                            context.Logger.LogLine(e.StackTrace); 
-                        }
-                    }
+                    await PublishMessageToClient(context, client, stream, apiClient);
                 }
                 
                 return new APIGatewayProxyResponse
@@ -105,13 +68,43 @@ namespace Publish
             }
             catch (Exception e)
             {
-                context.Logger.LogLine("Error connecting: " + e.Message);
+                context.Logger.LogLine("Error publishing messages: " + e.Message);
                 context.Logger.LogLine(e.StackTrace);
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 500,
-                    Body = $"Failed to connect: {e.Message}" 
+                    Body = $"Failed to publish messages: {e.Message}" 
                 };
+            }
+        }
+
+        private static async Task PublishMessageToClient(ILambdaContext context, Client client, MemoryStream stream,
+            AmazonApiGatewayManagementApiClient apiClient)
+        {
+            var postConnectionRequest = new PostToConnectionRequest
+            {
+                ConnectionId = client.ConnectionId,
+                Data = stream
+            };
+
+            try
+            {
+                context.Logger.LogLine($"Post to connection: {client.ConnectionId}");
+                stream.Position = 0;
+                await apiClient.PostToConnectionAsync(postConnectionRequest);
+            }
+            catch (AmazonServiceException e)
+            {
+                if (e.StatusCode == HttpStatusCode.Gone)
+                {
+                    context.Logger.LogLine($"Deleting gone connection: {client.ConnectionId}");
+                    await _clientConnectionService.DeleteClientConnectionByConnectionId(client.ConnectionId);
+                }
+                else
+                {
+                    context.Logger.LogLine($"Error posting message to {client.ConnectionId}: {e.Message}");
+                    context.Logger.LogLine(e.StackTrace); 
+                }
             }
         }
     }
